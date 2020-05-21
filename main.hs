c1ebc89762d3d698e8876c298704f8d5d8d1e031
zhvrnkov <v.zhvrnkv@gmail.com>
2020-05-21 23:41:09 +0600

    Add more functions and documentation

--- main.hs
+++ main.hs
@@ -1,6 +1,7 @@
 module ParseC where
 
-type Parser a b = [a] -> Either (b, [a]) String
+type ParserOutput a b = Either (b, [a]) String
+type Parser a b = [a] -> ParserOutput a b
 
 parser :: Eq a => a -> Parser a a
 parser _ [] = Right "Empty input"
@@ -11,30 +12,51 @@ parser x xs
 parsers :: Eq a => [a] -> [Parser a a]
 parsers = map parser
 
+-- run first parser, then run second parser.
+-- if both succeed, then output their parser outputs
 (.>>.) :: Parser a b -> Parser a c -> Parser a (b, c)
-(.>>.) p1 p2 = either (\(x, rstr) -> either (\(y, rrstr) -> Left ((x, y), rrstr)) (Right . id) $ p2 rstr) (Right .id) . p1
+(.>>.) p1 p2 = either (\(x, rstr) ->
+                         either (\(y, rrstr) -> Left ((x, y), rrstr))
+                         (Right . id) $ p2 rstr)
+                      (Right . id) . p1
 
+-- run both parsers.
+-- if both succeed, then output first parser output
 (.>>) :: Parser a b -> Parser a c -> Parser a b
 (.>>) p1 p2 = fst |>> (p1 .>>. p2)
 
+-- run both parsers
+-- if both succeed, then output second parser output
 (>>.) :: Parser a b -> Parser a c -> Parser a c
 (>>.) p1 p2 = snd |>> (p1 .>>. p2)
 
+-- run all parsers.
+-- if all succeed, then output second parser output
 pbetween :: Parser a b1 -> Parser a b2 -> Parser a c -> Parser a b2
 pbetween p1 p2 p3 = p1 >>. p2 .>> p3
 
+-- run first parser.
+-- if succeed -- output
+-- if failed -- run second parser
 (<|>) :: Parser a b -> Parser a b -> Parser a b
 (<|>) p1 p2 str = either (Left .id) (const $ either (Left . id) (Right . id) $ p2 str) $ p1 str
 
+-- run parser and map its output
 (|>>) :: (a -> b) -> Parser d a -> Parser d b
 (|>>) map p1 = either (\(x, str) -> Left (map x, str)) (Right . id) . p1
 
+-- try to run all parsers
+-- if any succeed -- output
+-- if all failed -- failed
 choice :: Eq a => [Parser a a] -> Parser a a
 choice (p:ps) = foldl (<|>) p ps
 
 anyOf :: Eq a => [a] -> Parser a a
-anyOf = choice . map parser
+anyOf = choice . parsers
 
+-- run all parsers in sequence
+-- if any failed -- failed
+-- if all succeed -- output the sequence
 psequence :: [Parser a a] -> Parser a [a]
 psequence parsers = foldl (\acc p -> (|>>) (\(x, y) -> x ++ y) $ (acc .>>. p)) p ps
    where (p:ps) = map ((:[]) |>>) parsers
@@ -45,3 +67,16 @@ psequence parsers = foldl (\acc p -> (|>>) (\(x, y) -> x ++ y) $ (acc .>>. p)) p
 
 (>>%) :: Parser d a -> b -> Parser d b
 (>>%) p x = (\_ -> x) |>> p
+
+many parser input = either success failure $ parser input
+  where success = (\(x, rinput) -> let manyOutput = (many parser rinput)
+                                   in (x:(fst manyOutput), snd manyOutput))
+        failure = (\_ -> ([], input)) 
+
+many1 parser = either success failure . parser
+  where success = (\(x, rinput) -> let manyOutput = many parser rinput
+                                   in Left (x:(fst manyOutput), snd manyOutput))
+        failure = (Right . id)
+
+pdefault :: a -> Parser a a
+pdefault x = (\input -> Left (x, input))
